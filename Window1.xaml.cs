738ff6e8832b08a5f935e0e11aef4d6298fb9bea
Shguts <shguts@mail.ru>
2021-04-02 23:47:02 +0300

    ZXCZXCZXCZXCZXCZXCZXC

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -61,23 +61,15 @@ namespace KSHG
                     case 2: break;
                     case 3: break;
                     case 4: break;
-                    default: { sourse2.DateofBirth = Convert.ToDateTime(PROV4);break;};
+                    default: { sourse2.DateofBirth = Convert.ToDateTime(PROV4); break; };
                 }
                 db.Users.Add(sourse2);
                 db.SaveChanges();
-
-            }           
-            using (kursRabEntities db = new kursRabEntities())
-            {
                 DataUsers sourse1 = new DataUsers();
-                Users sourse3 = new Users();
                 sourse1.LoginUs = PROV5;
                 sourse1.PasswordUs = PROV6;
-                var TEST1= db.DataUsers.Where(x => x.LoginUs == PROV5).Select(X => X.IDUser).FirstOrDefault(); 
+                int TEST1= db.Users.OrderByDescending(x=>x.IDUser).Select(X => X.IDUser).FirstOrDefault()+1;
                 sourse1.IDUser = TEST1;
-                //var TEST1 = db.DataUsers.Where(x => x.LoginUs == PROV5).Select(X => X.IDUser);
-                //sourse1.IDUser = Convert.ToInt32(TEST1);
-                db.Users.Add(sourse2);
                 db.DataUsers.Add(sourse1);
                 db.SaveChanges();
 
